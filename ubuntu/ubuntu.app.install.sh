2e8c62aff9c8386f7e89b9517db0f429b76be2c0
tainvecs <tainvecs@gmail.com>
2019-01-15 23:28:32 +0800

    1. comment the ip address in dymotd to make motd load faster 2. space_file function add $1 space to each line in $2 and output to $3

--- ubuntu/ubuntu.app.install.sh
+++ ubuntu/ubuntu.app.install.sh
@@ -27,6 +27,7 @@ sudo apt-get -y install tmux
 sudo apt-get -y install htop
 sudo apt-get -y install dialog
 sudo apt-get -y install locales
+sudo apt-get -y install iputils-ping
 sudo apt-get -y install less vim
 sudo apt-get -y install python3
 sudo apt-get -y install python3-pip python3-dev build-essential
