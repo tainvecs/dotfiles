07d67dfbc71a3274316467ee3f0c44286c2bd928
Chin-Ho Lin <tainvecs@gmail.com>
2022-04-15 19:36:26 +0800

    feat: update mac plugin for mac cleanup, destop show/hide file and afk function

--- plugins/mac/.mac.plugin.zsh
+++ plugins/mac/.mac.plugin.zsh
@@ -1,29 +1,45 @@
 if [[ `uname` = "Darwin" ]]; then
 
-    # defined for switching mac finder mode that
-    # -t for to show hidden files
-    # -f for not to show hidden files
-    macfm() {
+    function mac-cleanup() {
 
-        if [ "$1" = "-t" ]; then
+        # recursively delete `.DS_Store` files
+        find . -type f -name '*.DS_Store' -ls -delete
 
-            defaults write com.apple.finder AppleShowAllFiles TRUE
-            killall Finder
-        
-            echo "Finder show hidden file mode was setted to True."
-        
-        elif [ "$1" = "-f" ]; then
-        
-            defaults write com.apple.finder AppleShowAllFiles FALSE
-            killall Finder
-        
-            echo "Finder show hidden file mode was setted to False."
-        
-        else
-        
-            echo "Finder show hidden file mode is unknown."
-        
+        if type brew>/dev/null; then
+            brew doctor
+            brew cleanup -s
+            brew cleanup --prune=all
         fi
+
+    }
+
+    # show/hide hidden files
+    function mac-finder-show() {
+        defaults write com.apple.finder AppleShowAllFiles TRUE
+        killall Finder
+        echo "Finder show hidden file mode was setted to True."
+    }
+    function mac-finder-hide() {
+        defaults write com.apple.finder AppleShowAllFiles FALSE
+        killall Finder
+        echo "Finder show hidden file mode was setted to False."
+    }
+
+    # hide/show all desktop icons (useful when presenting)
+    function mac-desktop-show() {
+        defaults write com.apple.finder CreateDesktop TRUE
+        killall Finder
+        echo "Finder show hidden file mode was setted to True."
+    }
+    function mac-desktop-hide() {
+        defaults write com.apple.finder CreateDesktop FALSE
+        killall Finder
+        echo "Finder show hidden file mode was setted to False."
+    }
+
+    # afk
+    function afk(){
+        osascript -e 'tell app "System Events" to key code 12 using {control down, command down}'
     }
 
 fi
