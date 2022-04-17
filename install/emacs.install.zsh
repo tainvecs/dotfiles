91b36d0cdfd565c382471c71d70199f3bf83840c
Chin-Ho Lin <tainvecs@gmail.com>
2022-04-17 20:28:29 +0800

    fix: go install emacs depended packages for go version less than 1.16

--- install/emacs.install.zsh
+++ install/emacs.install.zsh
@@ -1,6 +1,7 @@
 DOTFILES_ROOT="$(dirname $(cd $(dirname $0) >/dev/null 2>&1; pwd -P;))"
-
 DOTFILES_HOME="$DOTFILES_ROOT/home"
+
+OS_TYPE=`uname`
 
 
 # load init
@@ -10,9 +11,21 @@ emacs --load "$DOTFILES_HOME/.emacs/init.el" --batch
 # install for go
 if type go >/dev/null; then
 
-    go install golang.org/x/tools/cmd/goimports@latest
-    go install github.com/rogpeppe/godef@latest
-    go install github.com/mdempsky/gocode@latest
-    go install golang.org/x/tools/gopls@latest
+    if [[ $OS_TYPE = "Darwin" ]]; then
+
+        go install golang.org/x/tools/cmd/goimports@latest
+        go install github.com/rogpeppe/godef@latest
+        go install github.com/mdempsky/gocode@latest
+        go install golang.org/x/tools/gopls@latest
+
+    elif [[ $OS_TYPE = "Linux" ]]; then
+
+        export GO111MODULE=on
+        go get golang.org/x/tools/cmd/goimports@latest
+        go get github.com/rogpeppe/godef@latest
+        go get github.com/mdempsky/gocode@latest
+        go get golang.org/x/tools/gopls@latest
+
+    fi
 
 fi
