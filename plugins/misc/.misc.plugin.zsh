56065e3280135fa9e506991c1cc744658b7e3863
Chin-Ho Lin <tainvecs@gmail.com>
2022-05-02 23:37:34 +0800

    chore: reorder misc plugin fuctinos and alias alphabetically

--- plugins/misc/.misc.plugin.zsh
+++ plugins/misc/.misc.plugin.zsh
@@ -1,49 +1,43 @@
 # ------------------------------------------------------------------------------
-# shell
+# alias
 # ------------------------------------------------------------------------------
 
 
-alias shell-which="echo $SHELL"
+# add space to command so that next word will be checked for alias substitution
+alias sudo="sudo "
+alias watch="watch -n 2 -c "
 
-function shell-change(){
-    shell_target=$(which $1)
-    $(chsh -s $shell_target)
-    clear
-    exec $shell_target
-}
+# alias for my freq typo
+alias celar="clear "
 
 
 # ------------------------------------------------------------------------------
-# PATH
+# cd
 # ------------------------------------------------------------------------------
 
 
-function path-ls(){
-    echo -e ${PATH//:/\\n}
-}
+# easier navigation: .., ..., ...., ....., ~ and -
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+alias -- -="cd -"
 
 
 # ------------------------------------------------------------------------------
-# link
+# checksums
 # ------------------------------------------------------------------------------
 
 
-function ln-prune-dry-run(){
-    find -L . -maxdepth 1 -type l -print
-}
+if type sha256sum >/dev/null; then
 
-function ln-prune(){
-    for f in `find -L . -maxdepth 1 -type l`; do unlink $f; done
-}
+    alias sha="sha256sum "
 
+elif type shasum >/dev/null; then
 
-# ------------------------------------------------------------------------------
-# cron
-# ------------------------------------------------------------------------------
+    alias sha="shasum -a 256 "
 
-
-alias cron-edit="crontab -e "
-alias cron-edit-sudo="sudo crontab -e "
+fi
 
 
 # ------------------------------------------------------------------------------
@@ -61,6 +55,69 @@ function comp-ls() {
     done | sort
 
 }
+
+
+# ------------------------------------------------------------------------------
+# cron
+# ------------------------------------------------------------------------------
+
+
+alias cron-edit="crontab -e "
+alias cron-edit-sudo="sudo crontab -e "
+
+
+# ------------------------------------------------------------------------------
+# link
+# ------------------------------------------------------------------------------
+
+
+function ln-prune-dry-run(){
+    find -L . -maxdepth 1 -type l -print
+}
+
+function ln-prune(){
+    for f in `find -L . -maxdepth 1 -type l`; do unlink $f; done
+}
+
+
+# ------------------------------------------------------------------------------
+# PATH
+# ------------------------------------------------------------------------------
+
+
+function path-ls(){
+    echo -e ${PATH//:/\\n}
+}
+
+
+# ------------------------------------------------------------------------------
+# random number
+# ------------------------------------------------------------------------------
+
+
+if type shuf >/dev/null; then
+
+    function rand(){
+        shuf -i ${1:-0-99} -n ${2:-1}
+    }
+
+fi
+
+
+# ------------------------------------------------------------------------------
+# shell
+# ------------------------------------------------------------------------------
+
+
+alias shell-which="echo $SHELL"
+
+function shell-change(){
+    shell_target=$(which $1)
+    $(chsh -s $shell_target)
+    clear
+    exec $shell_target
+}
+
 
 # ------------------------------------------------------------------------------
 # timestamp
@@ -82,59 +139,3 @@ if type uuidgen >/dev/null; then
     alias uuid-gen="uuidgen "
 
 fi
-
-
-# ------------------------------------------------------------------------------
-# checksums
-# ------------------------------------------------------------------------------
-
-
-if type sha256sum >/dev/null; then
-
-    alias sha="sha256sum "
-
-elif type shasum >/dev/null; then
-
-    alias sha="shasum -a 256 "
-
-fi
-
-
-# ------------------------------------------------------------------------------
-# random number
-# ------------------------------------------------------------------------------
-
-
-if type shuf >/dev/null; then
-
-    function rand(){
-        shuf -i ${1:-0-99} -n ${2:-1}
-    }
-
-fi
-
-
-# ------------------------------------------------------------------------------
-# cd
-# ------------------------------------------------------------------------------
-
-
-# easier navigation: .., ..., ...., ....., ~ and -
-alias ..="cd .."
-alias ...="cd ../.."
-alias ....="cd ../../.."
-alias .....="cd ../../../.."
-alias -- -="cd -"
-
-
-# ------------------------------------------------------------------------------
-# misc alias
-# ------------------------------------------------------------------------------
-
-
-# add space to command so that next word will be checked for alias substitution
-alias sudo="sudo "
-alias watch="watch -n 2 -c "
-
-# alias for my freq typo
-alias celar="clear "
