45cc07e8f9864b9b4092e4b6e36b943c00645587
Chin-Ho, Lin <tainvecs@gmail.com>
2022-05-03 14:37:03 +0800

    fix: curl instead of wget for download script (#13)

--- install/download.zsh
+++ install/download.zsh
@@ -11,8 +11,8 @@ ITERM_COLORS_DST="$DOTFILES_RESOURCES/iterm"
 mkdir -p $ITERM_COLORS_DST
 
 if [[ ! -f "$ITERM_COLORS_DST/Solarized High Contrast Dark.itermcolors" ]]; then
-    wget -O "$ITERM_COLORS_DST/Solarized High Contrast Dark.itermcolors" \
-         "https://raw.githubusercontent.com/mbadolato/iTerm2-Color-Schemes/master/schemes/Solarized%20Dark%20Higher%20Contrast.itermcolors"
+    curl "https://raw.githubusercontent.com/mbadolato/iTerm2-Color-Schemes/master/schemes/Solarized%20Dark%20Higher%20Contrast.itermcolors"\
+         -o "$ITERM_COLORS_DST/Solarized High Contrast Dark.itermcolors"
 fi
 
 
@@ -25,43 +25,43 @@ FONTS_DST="$DOTFILES_RESOURCES/fonts"
 mkdir -p $FONTS_DST
 
 if [[ ! -f "$FONTS_DST/all-the-icons.ttf" ]]; then
-    wget -O "$FONTS_DST/all-the-icons.ttf" \
-         "https://github.com/domtronn/all-the-icons.el/blob/master/fonts/all-the-icons.ttf?raw=true"
+    curl "https://github.com/domtronn/all-the-icons.el/blob/master/fonts/all-the-icons.ttf?raw=true" \
+         -o "$FONTS_DST/all-the-icons.ttf"
 fi
 
 if [[ ! -f "$FONTS_DST/file-icons.ttf" ]]; then
-    wget -O "$FONTS_DST/file-icons.ttf" \
-         "https://github.com/domtronn/all-the-icons.el/blob/master/fonts/file-icons.ttf?raw=true"
+    curl "https://github.com/domtronn/all-the-icons.el/blob/master/fonts/file-icons.ttf?raw=true" \
+         -o "$FONTS_DST/file-icons.ttf"
 fi
 
 if [[ ! -f "$FONTS_DST/fontawesome.ttf" ]]; then
-    wget -O "$FONTS_DST/fontawesome.ttf" \
-         "https://github.com/domtronn/all-the-icons.el/blob/master/fonts/fontawesome.ttf?raw=true"
+    curl "https://github.com/domtronn/all-the-icons.el/blob/master/fonts/fontawesome.ttf?raw=true" \
+         -o "$FONTS_DST/fontawesome.ttf"
 fi
 
 if [[ ! -f "$FONTS_DST/material-design-icons.ttf" ]]; then
-    wget -O "$FONTS_DST/material-design-icons.ttf" \
-         "https://github.com/domtronn/all-the-icons.el/blob/master/fonts/material-design-icons.ttf?raw=true"
+    curl "https://github.com/domtronn/all-the-icons.el/blob/master/fonts/material-design-icons.ttf?raw=true" \
+         -o "$FONTS_DST/material-design-icons.ttf"
 fi
 
 if [[ ! -f "$FONTS_DST/octicons.ttf" ]]; then
-    wget -O "$FONTS_DST/octicons.ttf" \
-         "https://github.com/domtronn/all-the-icons.el/blob/master/fonts/octicons.ttf?raw=true"
+    curl "https://github.com/domtronn/all-the-icons.el/blob/master/fonts/octicons.ttf?raw=true" \
+         -o "$FONTS_DST/octicons.ttf"
 fi
 
 if [[ ! -f "$FONTS_DST/weathericons.ttf" ]]; then
-    wget -O "$FONTS_DST/weathericons.ttf" \
-         "https://github.com/domtronn/all-the-icons.el/blob/master/fonts/weathericons.ttf?raw=true"
+    curl "https://github.com/domtronn/all-the-icons.el/blob/master/fonts/weathericons.ttf?raw=true" \
+         -o "$FONTS_DST/weathericons.ttf"
 fi
 
 if [[ ! -f "$FONTS_DST/MesloLGS NF Regular.ttf" ]]; then
-    wget -O "$FONTS_DST/MesloLGS NF Regular.ttf" \
-         "https://github.com/romkatv/powerlevel10k-media/blob/master/MesloLGS%20NF%20Regular.ttf?raw=true"
+    curl "https://github.com/romkatv/powerlevel10k-media/blob/master/MesloLGS%20NF%20Regular.ttf?raw=true" \
+         -o "$FONTS_DST/MesloLGS NF Regular.ttf"
 fi
 
 if [[ ! -f "$FONTS_DST/SourceCodePro-Medium.ttf" ]]; then
-    wget -O "$FONTS_DST/SourceCodePro-Medium.ttf" \
-         "https://github.com/adobe-fonts/source-code-pro/blob/release/TTF/SourceCodePro-Medium.ttf?raw=true"
+    curl "https://github.com/adobe-fonts/source-code-pro/blob/release/TTF/SourceCodePro-Medium.ttf?raw=true" \
+         -o "$FONTS_DST/SourceCodePro-Medium.ttf"
 fi
 
 
@@ -74,8 +74,8 @@ DELTA_THEME_DST="$DOTFILES_RESOURCES/git"
 mkdir -p $DELTA_THEME_DST
 
 if [[ ! -f "$DELTA_THEME_DST/themes.gitconfig" ]]; then
-    wget -O "$DELTA_THEME_DST/themes.gitconfig" \
-         "https://raw.githubusercontent.com/dandavison/delta/master/themes.gitconfig"
+    curl "https://raw.githubusercontent.com/dandavison/delta/master/themes.gitconfig" \
+         -o "$DELTA_THEME_DST/themes.gitconfig"
 fi
 
 
@@ -88,6 +88,6 @@ VIM_COLORS_DST="$DOTFILES_RESOURCES/vim/colors"
 mkdir -p $VIM_COLORS_DST
 
 if [[ ! -f "$VIM_COLORS_DST/desertEx.vim" ]]; then
-    wget -O "$VIM_COLORS_DST/desertEx.vim" \
-         "https://raw.githubusercontent.com/vim-scripts/desertEx/master/colors/desertEx.vim"
+    curl "https://raw.githubusercontent.com/vim-scripts/desertEx/master/colors/desertEx.vim" \
+         -o "$VIM_COLORS_DST/desertEx.vim"
 fi
