806fc84956e9742e50b0f5c9fa1a60cae535523c
Chin-Ho Lin <tainvecs@gmail.com>
2022-05-18 00:48:31 +0800

    refactor: move config file paths into arrays

--- scripts/config.build.zsh
+++ scripts/config.build.zsh
@@ -14,7 +14,7 @@ DOTFILES_RESOURCES="$DOTFILES_ROOT/resources"
 
 
 # ------------------------------------------------------------------------------
-# local/config_template
+# copy local/config_template to local/config
 # ------------------------------------------------------------------------------
 
 
@@ -59,47 +59,84 @@ if [[ $DOTFILES_APPLY_LOCAL_CONFIG_TEMPLATES = true ]]; then
         done
 
     done
-
     unsetopt nullglob
 
 fi
 
 
 # ------------------------------------------------------------------------------
-# aws
+# config -> local/config
 # ------------------------------------------------------------------------------
 
 
-# config -> local
-AWS_LOCAL_CONFIG_SRC="$DOTFILES_LOCAL/config/aws/config"
-AWS_LOCAL_CONFIG_DST="$DOTFILES_CONFIG/aws/config"
-[[ -L $AWS_LOCAL_CONFIG_DST || -f $AWS_LOCAL_CONFIG_DST ]] || ln -s $AWS_LOCAL_CONFIG_SRC $AWS_LOCAL_CONFIG_DST
+DOTFILES_LOCAL_CONFIG="$DOTFILES_LOCAL/config"
+mkdir -p "$DOTFILES_CONFIG"
 
+# local config
+declare -a LOCAL_CONFIG_PATH_ARR=(
 
-# ------------------------------------------------------------------------------
-# docker
-# ------------------------------------------------------------------------------
+    # aws
+    "$DOTFILES_LOCAL/config/aws/config"
 
+    # docker
+    "$DOTFILES_LOCAL/config/docker/config.json"
+    "$DOTFILES_LOCAL/config/docker/daemon.json"
 
-# config -> local
-DOCKER_LOCAL_CONFIG_SRC="$DOTFILES_LOCAL/config/docker/config.json"
-DOCKER_LOCAL_CONFIG_DST="$DOTFILES_CONFIG/docker/config.json"
-[[ -L $DOCKER_LOCAL_CONFIG_SRC || -f $DOCKER_LOCAL_CONFIG_DST ]] || ln -s $DOCKER_LOCAL_CONFIG_SRC $DOCKER_LOCAL_CONFIG_DST
+    # emacs
+    "$DOTFILES_LOCAL/config/emacs/init.el.local"
 
-DOCKERD_LOCAL_CONFIG_SRC="$DOTFILES_LOCAL/config/docker/daemon.json"
-DOCKERD_LOCAL_CONFIG_DST="$DOTFILES_CONFIG/docker/daemon.json"
-[[ -L $DOCKERD_LOCAL_CONFIG_SRC || -f $DOCKERD_LOCAL_CONFIG_DST ]] || ln -s $DOCKERD_LOCAL_CONFIG_SRC $DOCKERD_LOCAL_CONFIG_DST
+    # git: delta
+    "$DOTFILES_LOCAL/config/git/config"
 
+    # iterm2
+    "$DOTFILES_LOCAL/config/iterm2/keymap.itermkeymap"
+    "$DOTFILES_LOCAL/config/iterm2/profile-default.json"
 
-# ------------------------------------------------------------------------------
-# emacs
-# ------------------------------------------------------------------------------
+    # lazydocker
+    "$DOTFILES_LOCAL/config/lazydocker/config.yaml"
 
+    # p10k
+    "$DOTFILES_LOCAL/config/p10k/.p10k.zsh"
 
-# config -> local
-EMACS_LOCAL_CONFIG_SRC="$DOTFILES_LOCAL/config/emacs/init.el.local"
-EMACS_LOCAL_CONFIG_DST="$DOTFILES_CONFIG/emacs/init.el.local"
-[[ -L $EMACS_LOCAL_CONFIG_SRC || -f $EMACS_LOCAL_CONFIG_DST ]] || ln -s $EMACS_LOCAL_CONFIG_SRC $EMACS_LOCAL_CONFIG_DST
+    # peco
+    "$DOTFILES_LOCAL/config/peco/config.json"
+
+    # python
+    "$DOTFILES_LOCAL/config/python/.pythonrc"
+
+    # ssh
+    "$DOTFILES_LOCAL/config/ssh/config"
+
+    # tmux
+    "$DOTFILES_LOCAL/config/tmux/.tmux.conf.local"
+
+    # vim
+    "$DOTFILES_LOCAL/config/vim/.vimrc.local"
+
+    # zsh
+    "$DOTFILES_LOCAL/config/zsh/.zshenv.local"
+    "$DOTFILES_LOCAL/config/zsh/.zshrc_initialize.zsh.local"
+    "$DOTFILES_LOCAL/config/zsh/.zshrc_apps.zsh.local"
+    "$DOTFILES_LOCAL/config/zsh/.zshrc_plugins.zsh.local"
+    "$DOTFILES_LOCAL/config/zsh/.zshrc_finalize.zsh.local"
+
+)
+
+setopt nullglob
+for src_path in $LOCAL_CONFIG_PATH_ARR; do
+
+    dst_path=$(echo $src_path | sed "s#${DOTFILES_LOCAL_CONFIG}#${DOTFILES_CONFIG}#")
+    dst_dir=$(dirname "$dst_path")
+
+    if [[ ! -d $dst_dir ]]; then
+        echo "missing directory $dst_dir"
+        mkdir -p $dst_dir
+    fi
+
+    [[ -L $dst_path || -f $dst_path ]] || ln -s $src_path $dst_path
+
+done
+unsetopt nullglob
 
 
 # ------------------------------------------------------------------------------
@@ -112,70 +149,6 @@ GIT_THEME_SRC="$DOTFILES_RESOURCES/git/themes.gitconfig"
 GIT_THEME_DST="$DOTFILES_CONFIG/git/themes.gitconfig"
 [[ -f $GIT_THEME_SRC ]] && [[ ! -f $GIT_THEME_DST ]] && ln -s $GIT_THEME_SRC $GIT_THEME_DST
 
-# config -> local
-GIT_LOCAL_CONFIG_SRC="$DOTFILES_LOCAL/config/git/config"
-GIT_LOCAL_CONFIG_DST="$DOTFILES_CONFIG/git/config"
-[[ -L $GIT_LOCAL_CONFIG_DST || -f $GIT_LOCAL_CONFIG_DST ]] || ln -s $GIT_LOCAL_CONFIG_SRC $GIT_LOCAL_CONFIG_DST
-
-
-# ------------------------------------------------------------------------------
-# iterm2
-# ------------------------------------------------------------------------------
-
-
-# config -> local
-ITERM_KEYMAP_LOCAL_SRC="$DOTFILES_LOCAL/config/iterm2/keymap.itermkeymap"
-ITERM_KEYMAP_LOCAL_DST="$DOTFILES_CONFIG/iterm2/keymap.itermkeymap"
-[[ -L $ITERM_KEYMAP_LOCAL_DST || -f $ITERM_KEYMAP_LOCAL_DST ]] || ln -s $ITERM_KEYMAP_LOCAL_SRC $ITERM_KEYMAP_LOCAL_DST
-
-ITERM_PROFILE_LOCAL_SRC="$DOTFILES_LOCAL/config/iterm2/profile-default.json"
-ITERM_PROFILE_LOCAL_DST="$DOTFILES_CONFIG/iterm2/profile-default.json"
-[[ -L $ITERM_PROFILE_LOCAL_DST || -f $ITERM_PROFILE_LOCAL_DST ]] || ln -s $ITERM_PROFILE_LOCAL_SRC $ITERM_PROFILE_LOCAL_DST
-
-
-# ------------------------------------------------------------------------------
-# lazydocker
-# ------------------------------------------------------------------------------
-
-
-# config -> local
-LAYZDOCKER_LOCAL_CONFIG_SRC="$DOTFILES_LOCAL/config/lazydocker/config.yaml"
-LAYZDOCKER_LOCAL_CONFIG_DST="$DOTFILES_CONFIG/lazydocker/config.yaml"
-[[ -L $LAYZDOCKER_LOCAL_CONFIG_DST || -f $LAYZDOCKER_LOCAL_CONFIG_DST ]] || ln -s $LAYZDOCKER_LOCAL_CONFIG_SRC $LAYZDOCKER_LOCAL_CONFIG_DST
-
-
-# ------------------------------------------------------------------------------
-# p10k
-# ------------------------------------------------------------------------------
-
-
-# config -> local
-P10K_LOCAL_CONFIG_SRC="$DOTFILES_LOCAL/config/p10k/.p10k.zsh"
-P10K_LOCAL_CONFIG_DST="$DOTFILES_CONFIG/p10k/.p10k.zsh"
-[[ -L $P10K_LOCAL_CONFIG_DST || -f $P10K_LOCAL_CONFIG_DST ]] || ln -s $P10K_LOCAL_CONFIG_SRC $P10K_LOCAL_CONFIG_DST
-
-
-# ------------------------------------------------------------------------------
-# peco
-# ------------------------------------------------------------------------------
-
-
-# config -> local
-PECO_LOCAL_CONFIG_SRC="$DOTFILES_LOCAL/config/peco/config.json"
-PECO_LOCAL_CONFIG_DST="$DOTFILES_CONFIG/peco/config.json"
-[[ -L $PECO_LOCAL_CONFIG_DST || -f $PECO_LOCAL_CONFIG_DST ]] || ln -s $PECO_LOCAL_CONFIG_SRC $PECO_LOCAL_CONFIG_DST
-
-
-# ------------------------------------------------------------------------------
-# python
-# ------------------------------------------------------------------------------
-
-
-# config -> local
-PYTHON_LOCAL_CONFIG_SRC="$DOTFILES_LOCAL/config/python/.pythonrc"
-PYTHON_LOCAL_CONFIG_DST="$DOTFILES_CONFIG/python/.pythonrc"
-[[ -L $PYTHON_LOCAL_CONFIG_DST || -f $PYTHON_LOCAL_CONFIG_DST ]] || ln -s $PYTHON_LOCAL_CONFIG_SRC $PYTHON_LOCAL_CONFIG_DST
-
 
 # ------------------------------------------------------------------------------
 # ssh
@@ -184,57 +157,3 @@ PYTHON_LOCAL_CONFIG_DST="$DOTFILES_CONFIG/python/.pythonrc"
 
 # ssh_keys
 mkdir -p "$DOTFILES_CONFIG/ssh/ssh_keys"
-
-# config -> local
-SSH_LOCAL_CONFIG_SRC="$DOTFILES_LOCAL/config/ssh/config"
-SSH_LOCAL_CONFIG_DST="$DOTFILES_CONFIG/ssh/config"
-[[ -L $SSH_LOCAL_CONFIG_DST || -f $SSH_LOCAL_CONFIG_DST ]] || ln -s $SSH_LOCAL_CONFIG_SRC $SSH_LOCAL_CONFIG_DST
-
-
-# ------------------------------------------------------------------------------
-# tmux
-# ------------------------------------------------------------------------------
-
-
-# config -> local
-TMUX_LOCAL_CONFIG_SRC="$DOTFILES_LOCAL/config/tmux/.tmux.conf.local"
-TMUX_LOCAL_CONFIG_DST="$DOTFILES_CONFIG/tmux/.tmux.conf.local"
-[[ -L $TMUX_LOCAL_CONFIG_DST || -f $TMUX_LOCAL_CONFIG_DST ]] || ln -s $TMUX_LOCAL_CONFIG_SRC $TMUX_LOCAL_CONFIG_DST
-
-
-# ------------------------------------------------------------------------------
-# vim
-# ------------------------------------------------------------------------------
-
-
-# config -> local
-VIMRC_LOCAL_CONFIG_SRC="$DOTFILES_LOCAL/config/vim/.vimrc.local"
-VIMRC_LOCAL_CONFIG_DST="$DOTFILES_CONFIG/vim/.vimrc.local"
-[[ -L $VIMRC_LOCAL_CONFIG_DST || -f $VIMRC_LOCAL_CONFIG_DST ]] || ln -s $VIMRC_LOCAL_CONFIG_SRC $VIMRC_LOCAL_CONFIG_DST
-
-
-# ------------------------------------------------------------------------------
-# zsh
-# ------------------------------------------------------------------------------
-
-
-# config -> local
-ZSHENV_LOCAL_CONFIG_SRC="$DOTFILES_LOCAL/config/zsh/.zshenv.local"
-ZSHENV_LOCAL_CONFIG_DST="$DOTFILES_CONFIG/zsh/.zshenv.local"
-[[ -L $ZSHENV_LOCAL_CONFIG_DST || -f $ZSHENV_LOCAL_CONFIG_DST ]] || ln -s $ZSHENV_LOCAL_CONFIG_SRC $ZSHENV_LOCAL_CONFIG_DST
-
-ZSHRC_INIT_LOCAL_CONFIG_SRC="$DOTFILES_LOCAL/config/zsh/.zshrc_initialize.zsh.local"
-ZSHRC_INIT_LOCAL_CONFIG_DST="$DOTFILES_CONFIG/zsh/.zshrc_initialize.zsh.local"
-[[ -L $ZSHRC_INIT_LOCAL_CONFIG_DST || -f $ZSHRC_INIT_LOCAL_CONFIG_DST ]] || ln -s $ZSHRC_INIT_LOCAL_CONFIG_SRC $ZSHRC_INIT_LOCAL_CONFIG_DST
-
-ZSHRC_APPS_LOCAL_CONFIG_SRC="$DOTFILES_LOCAL/config/zsh/.zshrc_apps.zsh.local"
-ZSHRC_APPS_LOCAL_CONFIG_DST="$DOTFILES_CONFIG/zsh/.zshrc_apps.zsh.local"
-[[ -L $ZSHRC_APPS_LOCAL_CONFIG_DST || -f $ZSHRC_APPS_LOCAL_CONFIG_DST ]] || ln -s $ZSHRC_APPS_LOCAL_CONFIG_SRC $ZSHRC_APPS_LOCAL_CONFIG_DST
-
-ZSHRC_PLUGINS_LOCAL_CONFIG_SRC="$DOTFILES_LOCAL/config/zsh/.zshrc_plugins.zsh.local"
-ZSHRC_PLUGINS_LOCAL_CONFIG_DST="$DOTFILES_CONFIG/zsh/.zshrc_plugins.zsh.local"
-[[ -L $ZSHRC_PLUGINS_LOCAL_CONFIG_DST || -f $ZSHRC_PLUGINS_LOCAL_CONFIG_DST ]] || ln -s $ZSHRC_PLUGINS_LOCAL_CONFIG_SRC $ZSHRC_PLUGINS_LOCAL_CONFIG_DST
-
-ZSHRC_FINAL_LOCAL_CONFIG_SRC="$DOTFILES_LOCAL/config/zsh/.zshrc_finalize.zsh.local"
-ZSHRC_FINAL_LOCAL_CONFIG_DST="$DOTFILES_CONFIG/zsh/.zshrc_finalize.zsh.local"
-[[ -L $ZSHRC_FINAL_LOCAL_CONFIG_DST || -f $ZSHRC_FINAL_LOCAL_CONFIG_DST ]] || ln -s $ZSHRC_FINAL_LOCAL_CONFIG_SRC $ZSHRC_FINAL_LOCAL_CONFIG_DST
