dec281b7b57a610492345e06a4c03633dda852b2
Chin-Ho Lin <tainvecs@gmail.com>
2022-09-29 21:11:36 +0800

    feat: update fail2ban with count and find

--- plugins/network/.network.plugin.zsh
+++ plugins/network/.network.plugin.zsh
@@ -23,7 +23,7 @@ if type fail2ban-client >/dev/null; then
 
     if type sqlite3 >/dev/null; then
 
-        function f2b-jails(){
+        function f2b-jails-ls(){
 
             if [[ $1 == '-h' || $1 == '--help' ]]; then
                 echo 'List all jails.'
@@ -37,7 +37,7 @@ if type fail2ban-client >/dev/null; then
             echo $res_str
         }
 
-        function f2b-baned(){
+        function f2b-baned-ls(){
 
             if [[ $1 == '-h' || $1 == '--help' ]]; then
                 echo 'List all baned ip.'
@@ -55,7 +55,7 @@ if type fail2ban-client >/dev/null; then
     # sshd
     if type sqlite3 >/dev/null; then
 
-        function f2b-sshd-baned(){
+        function f2b-sshd-baned-ls(){
 
             if [[ $1 == '-h' || $1 == '--help' ]]; then
                 echo 'List all sshd baned ip.'
@@ -67,6 +67,31 @@ if type fail2ban-client >/dev/null; then
 
             echo "JAIL\tIP"
             echo $res_str
+        }
+
+        function f2b-sshd-baned-find(){
+
+            if [[ $1 == '-h' || $1 == '--help' || $1 == '' ]]; then
+                echo 'Find an ip $1 in sshd jail.'
+                echo '$1: ip'
+                return 0
+            fi
+
+            raw_res_str=$(sudo sqlite3 /var/lib/fail2ban/fail2ban.sqlite3 "SELECT jail, ip FROM bans WHERE jail='sshd' and ip='$1';")
+            res_str=$(echo $raw_res_str | sed 's/|/\t/g')
+
+            echo "JAIL\tIP"
+            echo $res_str
+        }
+
+        function f2b-sshd-baned-count(){
+
+            if [[ $1 == '-h' || $1 == '--help' ]]; then
+                echo 'Count banned ip in sshd jail.'
+                return 0
+            fi
+
+            sudo sqlite3 /var/lib/fail2ban/fail2ban.sqlite3 "SELECT COUNT(*) FROM bans WHERE jail='sshd';"
         }
     fi
 
@@ -91,10 +116,10 @@ if type fail2ban-client >/dev/null; then
         sudo fail2ban-client status sshd
     }
 
-    function f2b-sshd-ban-ip(){
+    function f2b-sshd-ban(){
 
         if [[ $1 == '-h' || $1 == '--help' || $1 == '' ]]; then
-            echo 'sshd: ban an ip.'
+            echo 'sshd: ban an ip $1.'
             echo '$1: ip'
             return 0
         fi
@@ -102,10 +127,10 @@ if type fail2ban-client >/dev/null; then
         sudo fail2ban-client set sshd banip $1
     }
 
-    function f2b-sshd-unban-ip(){
+    function f2b-sshd-unban(){
 
         if [[ $1 == '-h' || $1 == '--help' || $1 == '' ]]; then
-            echo 'sshd: unban an ip.'
+            echo 'sshd: unban an ip $1.'
             echo '$1: ip'
             return 0
         fi
