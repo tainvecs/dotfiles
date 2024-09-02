43eb94c34a61fde8fb640e2ccfd948cc77814e82
Chin-Ho Lin <tainvecs@gmail.com>
2024-09-02 18:24:30 +0800

    fix: files download with curl
    * chore: add -fL for curl multiple URL transfers

--- scripts/resources.download.zsh
+++ scripts/resources.download.zsh
@@ -23,7 +23,7 @@ ITERM_COLORS_DST="$DOTFILES_RESOURCES/iterm"
 mkdir -p $ITERM_COLORS_DST
 
 if [[ ! -f "$ITERM_COLORS_DST/Solarized High Contrast Dark.itermcolors" ]]; then
-    curl "https://raw.githubusercontent.com/mbadolato/iTerm2-Color-Schemes/master/schemes/Solarized%20Dark%20Higher%20Contrast.itermcolors"\
+    curl -fL "https://raw.githubusercontent.com/mbadolato/iTerm2-Color-Schemes/master/schemes/Solarized%20Dark%20Higher%20Contrast.itermcolors"\
          -o "$ITERM_COLORS_DST/Solarized High Contrast Dark.itermcolors"
 fi
 
@@ -39,38 +39,38 @@ mkdir -p $FONTS_DST
 
 # for emacs
 if [[ ! -f "$FONTS_DST/all-the-icons.ttf" ]]; then
-    curl "https://github.com/domtronn/all-the-icons.el/blob/master/fonts/all-the-icons.ttf?raw=true" \
+    curl -fL "https://github.com/domtronn/all-the-icons.el/blob/master/fonts/all-the-icons.ttf?raw=true" \
          -o "$FONTS_DST/all-the-icons.ttf"
 fi
 
 if [[ ! -f "$FONTS_DST/file-icons.ttf" ]]; then
-    curl "https://github.com/domtronn/all-the-icons.el/blob/master/fonts/file-icons.ttf?raw=true" \
+    curl -fL "https://github.com/domtronn/all-the-icons.el/blob/master/fonts/file-icons.ttf?raw=true" \
          -o "$FONTS_DST/file-icons.ttf"
 fi
 
 if [[ ! -f "$FONTS_DST/fontawesome.ttf" ]]; then
-    curl "https://github.com/domtronn/all-the-icons.el/blob/master/fonts/fontawesome.ttf?raw=true" \
+    curl -fL "https://github.com/domtronn/all-the-icons.el/blob/master/fonts/fontawesome.ttf?raw=true" \
          -o "$FONTS_DST/fontawesome.ttf"
 fi
 
 if [[ ! -f "$FONTS_DST/material-design-icons.ttf" ]]; then
-    curl "https://github.com/domtronn/all-the-icons.el/blob/master/fonts/material-design-icons.ttf?raw=true" \
+    curl -fL "https://github.com/domtronn/all-the-icons.el/blob/master/fonts/material-design-icons.ttf?raw=true" \
          -o "$FONTS_DST/material-design-icons.ttf"
 fi
 
 if [[ ! -f "$FONTS_DST/octicons.ttf" ]]; then
-    curl "https://github.com/domtronn/all-the-icons.el/blob/master/fonts/octicons.ttf?raw=true" \
+    curl -fL "https://github.com/domtronn/all-the-icons.el/blob/master/fonts/octicons.ttf?raw=true" \
          -o "$FONTS_DST/octicons.ttf"
 fi
 
 if [[ ! -f "$FONTS_DST/weathericons.ttf" ]]; then
-    curl "https://github.com/domtronn/all-the-icons.el/blob/master/fonts/weathericons.ttf?raw=true" \
+    curl -fL "https://github.com/domtronn/all-the-icons.el/blob/master/fonts/weathericons.ttf?raw=true" \
          -o "$FONTS_DST/weathericons.ttf"
 fi
 
 # for iterms
 if [[ ! -f "$FONTS_DST/MesloLGS NF Regular.ttf" ]]; then
-    curl "https://github.com/romkatv/powerlevel10k-media/blob/master/MesloLGS%20NF%20Regular.ttf?raw=true" \
+    curl -fL "https://github.com/romkatv/powerlevel10k-media/blob/master/MesloLGS%20NF%20Regular.ttf?raw=true" \
          -o "$FONTS_DST/MesloLGS NF Regular.ttf"
 fi
 
@@ -101,7 +101,7 @@ mkdir -p $DELTA_THEME_DST
 
 if [[ ! -f "$DELTA_THEME_DST/themes.gitconfig" ]]; then
 
-    curl "https://raw.githubusercontent.com/dandavison/delta/master/themes.gitconfig" \
+    curl -fL "https://raw.githubusercontent.com/dandavison/delta/master/themes.gitconfig" \
          -o "$DELTA_THEME_DST/themes.gitconfig"
 
     # comment the missing syntax themem
@@ -127,6 +127,6 @@ VIM_COLORS_DST="$DOTFILES_RESOURCES/vim/colors"
 mkdir -p $VIM_COLORS_DST
 
 if [[ ! -f "$VIM_COLORS_DST/desertEx.vim" ]]; then
-    curl "https://raw.githubusercontent.com/vim-scripts/desertEx/master/colors/desertEx.vim" \
+    curl -fL "https://raw.githubusercontent.com/vim-scripts/desertEx/master/colors/desertEx.vim" \
          -o "$VIM_COLORS_DST/desertEx.vim"
 fi
