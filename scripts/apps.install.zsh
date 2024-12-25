a6a8fb8b864e3e1b34a206a8c4d50112bc118579
Chin-Ho Lin <tainvecs@gmail.com>
2024-12-25 22:31:23 +0800

    refactor: apt install elasticsearch with keyring instead of apt-key

--- scripts/apps.install.zsh
+++ scripts/apps.install.zsh
@@ -485,10 +485,13 @@ elif [[ $SYS_NAME = "linux" ]]; then
        ! { dpkg -l elasticsearch &>"/dev/null" }
     then
 
-        # install
-        wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+        # prerequisite
+        wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --dearmor -o /usr/share/keyrings/elasticsearch-keyring.gpg
         sudo_apt_install apt-transport-https
-        echo "deb https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-8.x.list
+        echo "deb [signed-by=/usr/share/keyrings/elasticsearch-keyring.gpg] https://artifacts.elastic.co/packages/8.x/apt stable main" | \
+            sudo tee /etc/apt/sources.list.d/elastic-8.x.list
+
+        # install
         sudo apt-get update && sudo_apt_install elasticsearch
 
         # start
