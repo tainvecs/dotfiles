5380e95d9a309f41d2d021957b970dfa1e563cff
Chin-Ho Lin <tainvecs@gmail.com>
2024-12-25 22:27:25 +0800

    fix: kubectl linux installation with architecture specified

--- scripts/apps.install.zsh
+++ scripts/apps.install.zsh
@@ -546,7 +546,7 @@ elif [[ $SYS_NAME = "linux" ]]; then
        ! { type kubectl >"/dev/null" } && \
        ! { dpkg -l kubectl &>"/dev/null" }
     then
-        curl -fLO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+        curl -fLO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/$SYS_ARCHT/kubectl"
         sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
         rm kubectl
     else
