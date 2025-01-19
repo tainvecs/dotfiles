30cb50b53e97cafef386283cb2bdc3ce683b6c61
Chin-Ho Lin <tainvecs@gmail.com>
2025-01-19 17:19:58 +0800

    feat: docker and docker-compose completion download and setup

--- config/zsh/.zshrc_apps.zsh
+++ config/zsh/.zshrc_apps.zsh
@@ -122,16 +122,16 @@ function _dotfiles_init_docker(){
 
     if type docker >"/dev/null"; then
 
-        # home
+        # ----- home
         local _docker_home_dir="${DOTFILES[HOME_DIR]}/.docker"
         [[ -d $_docker_home_dir ]] || mkdir -p $_docker_home_dir
 
-        # config
+        # ----- config
         # Location of the client config file config.json (default ~/.docker)
         export DOCKER_CONFIG=$_docker_home_dir
         export DOCKER_DEFAULT_PLATFORM="linux/$SYS_ARCHT"
 
-        # dockerd
+        # ----- dockerd
         if type dockerd >"/dev/null"; then
 
             # config
@@ -140,6 +140,28 @@ function _dotfiles_init_docker(){
                 alias dockerd="dockerd --config-file $_dockerd_config_file_path "
             fi
         fi
+
+        # ----- complete
+
+        # docker
+        local _docker_cmp_path="${DOTFILES[CONFIG_DIR]}/zsh/.zsh_complete/_docker"
+        local _docker_cmp_link="${DOTFILES[HOME_DIR]}/.zsh/.zsh_complete/_docker"
+
+        if [[ ! -f $_docker_cmp_path ]]; then
+            curl -fLo $_docker_cmp_path \
+                 "https://raw.githubusercontent.com/docker/cli/master/contrib/completion/zsh/_docker"
+        fi
+        [[ -L $_docker_cmp_link || -f $_docker_cmp_link ]] || ln -s $_docker_cmp_path $_docker_cmp_link
+
+        # docker compose
+        local _docker_comp_cmp_path="${DOTFILES[CONFIG_DIR]}/zsh/.zsh_complete/_docker-compose"
+        local _docker_comp_cmp_link="${DOTFILES[HOME_DIR]}/.zsh/.zsh_complete/_docker-compose"
+
+        if [[ ! -f $_docker_comp_cmp_path ]]; then
+            curl -fLo $_docker_comp_cmp_path \
+                 "https://raw.githubusercontent.com/docker/compose/1.29.2/contrib/completion/zsh/_docker-compose"
+        fi
+        [[ -L $_docker_comp_cmp_link || -f $_docker_comp_cmp_link ]] || ln -s $_docker_comp_cmp_path $_docker_comp_cmp_link
     fi
 }
 
